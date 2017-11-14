3d1e625a4b53d57e942111a7581e2fabd5b8c214
BR1312 <xiao_bairui@163.com>
2017-11-14 20:50:59 +0800

    aaa

---
+++ app/src/main/java/com/example/br1312/fragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.br1312.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+    /**
+     * 1.定义Fragment的子类 并加载一个布局文件
+     * 2.在布局文件中通过<fragment>指定自定义Fragment
+     * 3.我们的Activity必须继承于FragmentActivity
+     *
+     *  Fragment本质上都会生成一个FrameLayout 它加载的布局为其子布局
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/br1312/fragment/MyFragment1.java
@@ -0,0 +1,30 @@
+package com.example.br1312.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by BR1312 on 2017/10/16.
+ */
+
+public class MyFragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+       //加载布局得到View对象并返回
+
+        //创建一个视图对象， 设置数据并返回
+        TextView textView = new TextView(getActivity());
+        textView.setText("Fragment1111");
+        textView.setBackgroundColor(Color.RED);
+
+        return textView;
+    }
+}
